e6fafec5fa68a680c7ac6f7261e51ce756c74ec2
jacobiDaDSP <thugpost23@gmail.com>
2026-01-31 21:21:32 -0500

    Update token.py

--- config/token.py
+++ config/token.py
@@ -1,1 +1,6 @@
-Token = "MTQ2NjMxNjMyMzk3NjkwOTAxNw.GHxzz3.IGtP-J6gvfGC5bAAUJl-Hq9D1T3UgLrpxy2D5s"
+import os
+
+Token = os.getenv("DISCORD_TOKEN")
+
+if not Token:
+    raise RuntimeError("DISCORD_TOKEN is not set")
